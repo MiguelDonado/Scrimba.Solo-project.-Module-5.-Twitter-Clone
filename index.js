87e90e2a855da05c1e035ccce8a941bc1ae1328e
MiguelDonado <miguel.5799@hotmail.es>
2024-02-03 21:23:45 +0100

    Populate the app using JS, instead of hardcoded in html. And also make the require styling.

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 import { tweetsData } from './data.js'
-tweetsData
+
+const tweetsContainerEl = document.getElementById('tweets-container')
+
+function getTweetsData(){
+    let tweetsHtml = ``
+    tweetsData.forEach(function(tweet){
+        tweetsHtml+=`
+            <div class="tweet" id="${tweet.uuid}">
+                <div class="tweet-profile-img">
+                    <img src="${tweet.profilePic}" class="profile">
+                </div>
+                <div class="tweet-inner">
+                    <p class="tweet-handle">${tweet.handle}</p>
+                    <p class="tweet-area">${tweet.tweetText}</p>
+                    <div class="tweet-icon">
+                        <i class="fa-regular fa-comment-dots"></i>
+                        <i class="fa-solid fa-heart"></i>
+                        <i class="fa-solid fa-retweet"></i>
+                    </div>
+                </div>
+            </div>            
+        `   
+    })
+    tweetsContainerEl.innerHTML = tweetsHtml
+}
+
+getTweetsData()
