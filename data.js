51e1825c86e4c9b6b4706daae2c33f2663359821
MiguelDonado <miguel.5799@hotmail.es>
2024-02-03 10:09:40 +0100

    Structure and styling of the header and first section. Also data population

---
+++ data.js
@@ -0,0 +1,52 @@
+export const tweetsData = [
+    {
+        handle:`@Guti`,
+        profilePic:`images/guti.jpg`,
+        likes: 14,
+        retweets: 10,
+        tweetText: `Al que no le guste, que se vaya a coger amapolas 🌼🌼 al campo.`,
+        replies: [
+                    {
+                handle:`@loboCarrasco`,
+                profilePic: `images/lobo.png`,
+                tweetText: `Ganar una liga⚽, es más dificil que ganar la Champions.⚽`        
+                },
+                    {
+                handle:`@juanmaRodriguez`,
+                profilePic:`images/juanma.png`,
+                tweetText:`El tiburón 🦈🦈🦈🦈`    
+                }
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid:'4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },
+    {
+        handle:`@Cr7`,
+        profilePic:`images/ronaldo.jpg`,
+        likes:845,
+        retweets:7,
+        tweetText: `Porque soy guapo, soy rico y juego bien al fútbol, por eso me tienen envidia.`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid:'3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+    {
+        handle:`@Zidane`,
+        profilePic:`images/zidane.jpg`,
+        likes: 99,
+        retweets: 3,
+        tweetText: `Karim 😾😾 es muy bueno.`,
+        replies: [
+                {
+                handle:`@TitoFloren`,
+                profilePic: `images/florentino.png`,
+                tweetText: `Tranquilo.🧘‍♂️🧘‍♂️`        
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid:'8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },
+]
